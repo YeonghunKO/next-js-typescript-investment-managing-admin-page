915c4f5f6de777a771be0b54b6654aa5335fca50
YeonghunKO <yhko1988@gmail.com>
2022-12-02 13:31:29 +0900

    fix : story를 인식하지 못하는 문제
    style관련된 파일은 상대경로를 사용하여 불러옴.
    
    shared 절대경로 추가
    
    그외 전역 상태 관리 provider 추가

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from 'styled-components';
-import { Theme } from '@components/particles/Theme';
-import { GlobalStyles } from '@components/particles/GlobalStyles';
+import { Theme } from '../components/particles/Theme';
+import GlobalStyles from '../components/particles/GlobalStyles';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,11 +15,24 @@ export const parameters = {
   },
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // suspense: true,
+    },
+  },
+});
+
 const withThemeProvider = Story => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <Story />
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={true} />
+          <Story />
+        </QueryClientProvider>
+      </RecoilRoot>
     </ThemeProvider>
   );
 };
