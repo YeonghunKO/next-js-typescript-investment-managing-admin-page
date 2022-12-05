1210b1d6156c3496dde142af1e66f1d70ed3c8b7
YeonghunKO <yhko1988@gmail.com>
2022-12-05 21:49:26 +0900

    fix : submit 타입은 synthetic하게 해야 유연해짐

--- type/organisms/LoginInputForm.ts
+++ type/organisms/LoginInputForm.ts
@@ -1,6 +1,8 @@
+import type { authInputProps } from '@type/templates/login';
+import type { FieldErrorsImpl, UseFormRegister } from 'react-hook-form';
+
 interface LoginInputFormType {
-  register: any;
-  errors: any;
+  onSubmit?: (e: React.SyntheticEvent) => void;
   [key: string]: any;
 }
 
