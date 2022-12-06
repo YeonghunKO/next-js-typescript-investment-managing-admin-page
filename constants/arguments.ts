4868257832f70894d0f7157897bd7dfc09d4fb73
YeonghunKO <yhko1988@gmail.com>
2022-12-06 14:42:28 +0900

    fix : 상수 객체의 타입을 값으로 명시

--- constants/arguments.ts
+++ constants/arguments.ts
@@ -9,14 +9,14 @@ const COLOR_VARIANTS: Record<Uppercase<INPUT_VARIANT>, INPUT_VARIANT> = {
   TERTIARY: 'tertiary',
 };
 
-const LOGIN_INPUT_TYPE: { TEXT: 'text'; PASSWORD: 'password' } = {
+const LOGIN_INPUT_TYPE = {
   TEXT: 'text',
   PASSWORD: 'password',
-};
+} as const;
 
-const LOGIN_INPUT_CONTENT: { EMAIL: 'email'; PASSWORD: 'password' } = {
+const LOGIN_INPUT_CONTENT = {
   EMAIL: 'email',
   PASSWORD: 'password',
-};
+} as const;
 
 export { COLOR_VARIANTS, LOGIN_INPUT_TYPE, LOGIN_INPUT_CONTENT };
