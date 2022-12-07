63ca49aa0b1593d51da38af0acda71a4c92854be
YeonghunKO <yhko1988@gmail.com>
2022-12-07 22:51:21 +0900

    fix : access token을 request 에 포함

--- lib/api/common/customAxios.ts
+++ lib/api/common/customAxios.ts
@@ -1,8 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import Cookies from 'universal-cookie';
 // import Cookies from 'univer'
 
 const clientAPI = axios.create({
   baseURL: 'http://localhost:4000',
+});
+
+clientAPI.interceptors.request.use((config: AxiosRequestConfig<any>) => {
+  const cookies = new Cookies();
+  const accessToken = cookies.get('access_token');
+
+  if (!!accessToken && config.headers) {
+    config.headers.Authorization = 'Bearer ' + accessToken;
+  }
+
+  return config;
 });
 
 export default clientAPI;
