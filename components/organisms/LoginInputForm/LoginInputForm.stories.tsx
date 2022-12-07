4dab76ec5a657a5f48efde7bc9b0167ef5998ac2
YeonghunKO <yhko1988@gmail.com>
2022-12-07 22:49:35 +0900

    fix : input data pass

--- components/organisms/LoginInputForm/LoginInputForm.stories.tsx
+++ components/organisms/LoginInputForm/LoginInputForm.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import LoginInputForm from './LoginInputForm';
+import { LOGIN_DATA } from 'data/login';
 
 export default {
   title: 'Organisms/LoginInputForm',
@@ -12,4 +13,6 @@ const Template: ComponentStory<typeof LoginInputForm> = args => (
 
 export const Basic = Template.bind({});
 
-Basic.args = {};
+Basic.args = {
+  data: LOGIN_DATA,
+};
