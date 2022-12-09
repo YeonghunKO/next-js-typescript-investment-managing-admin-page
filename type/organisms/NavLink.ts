6b31068ae03bbaab076a8c32bb45be27f1e84b35
YeonghunKO <yhko1988@gmail.com>
2022-12-09 22:27:16 +0900

    feat : NavLink 타입

---
+++ type/organisms/NavLink.ts
@@ -0,0 +1,12 @@
+import { IconsKey } from '@components/atoms/Icons/Icon';
+import { ReactNode } from 'react';
+
+interface NavLinkType {
+  children: ReactNode;
+  icon?: { type: keyof IconsKey; size?: string };
+  href: string;
+  isActive: boolean;
+  [key: string]: any;
+}
+
+export type { NavLinkType };
