a6e9197cf757a0797cc8e1817f8318338f80f450
YeonghunKO <yhko1988@gmail.com>
2022-12-09 22:26:37 +0900

    refactor : 자동완성을 위해

--- constants/arguments.ts
+++ constants/arguments.ts
@@ -1,13 +1,11 @@
-import type { INPUT_VARIANT } from '@type/atoms/LoginInput';
-
-export const COLOR_VARIANTS: Record<string, string> = {
+export const COLOR_VARIANTS = {
   PRIMARY: 'primary',
   PRIMARY100: 'primary100',
   PRIMARY500: 'primary500',
   PRIMARY900: 'primary900',
   SECONDARY: 'secondary',
   TERTIARY: 'tertiary',
-};
+} as const;
 
 export const LOGIN_INPUT_TYPE = {
   TEXT: 'text',
