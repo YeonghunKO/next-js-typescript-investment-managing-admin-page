792629209613940511aa3af104d8a0ee7ab7a963
YeonghunKO <yhko1988@gmail.com>
2022-12-11 22:49:07 +0900

    feat : header, sider 데이터 추가

---
+++ data/accounts.ts
@@ -0,0 +1,14 @@
+import { ROUTER } from '@constants/router';
+
+export const ACCOUNTS_DATA = {
+  header: [
+    {
+      title: '투자계좌',
+      icons: ['Bell', 'Question', 'User'],
+    },
+  ],
+  sider: [
+    { text: '계좌목록', href: ROUTER.ACCOUNTS, icon: 'Chart' as const },
+    { text: '사용자목록', href: ROUTER.USERS, icon: 'UserCircle' as const },
+  ],
+};
