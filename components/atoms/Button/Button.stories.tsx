dadd206434af2672fdbc0b2ade0fa9af38d96ace
YeonghunKO <yhko1988@gmail.com>
2022-12-12 20:39:24 +0900

    fix : icon args 변경

--- components/atoms/Button/Button.stories.tsx
+++ components/atoms/Button/Button.stories.tsx
@@ -1,16 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Icons from '../Icons';
 import Button from './Button';
 
 export default {
   title: 'Atoms/Button',
   component: Button,
-  argTypes: {
-    icon: {
-      options: [...Object.keys(Icons)],
-      control: { type: 'select' }, // Automatically inferred when 'options' is defined
-    },
-  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
@@ -19,6 +12,6 @@ export const Basic = Template.bind({});
 
 Basic.args = {
   children: '버튼',
-  width: '10%',
-  icon: 'Bell',
+  width: 'auto',
+  icon: { type: 'Bell' },
 };
