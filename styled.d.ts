8284126e51a99286aa4aee2480592e7937d08e47
YeonghunKO <yhko1988@gmail.com>
2022-12-14 09:59:02 +0900

    style : grey50을 더 연하게

--- styled.d.ts
+++ styled.d.ts
@@ -9,7 +9,7 @@ declare module 'styled-components' {
     white: '#ffffff';
     offWhite: '#F0F0F0';
     grey: '#a6a6a6';
-    grey50: '#ededed';
+    grey50: '#f7f7f7';
     grey100: '#d9d9d9';
     grey200: '#bfbfbf';
     grey300: '#a6a6a6';
