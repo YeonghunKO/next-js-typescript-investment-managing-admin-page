c4e67d22876eb648d266c37f7b42a44b513fc4e4
YeonghunKO <yhko1988@gmail.com>
2022-12-17 14:56:58 +0900

    fix : layout에 data넘겨줌

--- pages/users.tsx
+++ pages/users.tsx
@@ -2,14 +2,17 @@ import Layout from '@components/organisms/Layout';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { accountDataState } from 'store/accountDataAtoms';
 
 const users: NextPage = () => {
+  const data = useRecoilValue(accountDataState);
   return (
     <>
       <Head>
         <title>사용자목록</title>
       </Head>
-      <Layout>
+      <Layout {...data}>
         <div>사용자목록</div>
       </Layout>
     </>
