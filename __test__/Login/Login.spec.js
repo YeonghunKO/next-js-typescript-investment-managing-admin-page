bd386682aa99c8053740faa6be7cb6c232b07a4f
Sinkyo <yhko1988@gmail.com>
2023-08-03 15:03:41 +0900

    fix : 찾을 수 없는 dom을 테스트 하려고 queryBy를 사용함

--- __test__/Login/Login.spec.js
+++ __test__/Login/Login.spec.js
@@ -37,12 +37,12 @@ describe("로그인 페이지 테스트 - id: sinkyo@gmail.com , password: test1
     pushMock.mockClear();
   });
 
-  it("id, password 올바르게 입력후 성공케이스", async () => {
+  it.only("id, password 올바르게 입력후 성공케이스", async () => {
     const emailInput = getInputByPlaceHolder("이메일 입력");
     const passwordInput = getInputByPlaceHolder("비밀번호 입력");
 
     act(() => {
-      user.type(emailInput, "sinkyo@gmails.com");
+      user.type(emailInput, "sinkyo@gmail.com");
       user.type(passwordInput, "test123");
     });
 
@@ -50,10 +50,15 @@ describe("로그인 페이지 테스트 - id: sinkyo@gmail.com , password: test1
 
     expect(pushMock).toHaveBeenCalledTimes(1);
 
-    // 올바른 사용자이므로 에러 버튼이 나타나지 않음
-    const errorButton = await findByElementByRole("error-button");
+    // 올바른 사용자이므로 에러 버튼이 나타나지 않음, queryBy api를 사용해야 element가 없을경우 null이 리턴되어 테스트 가능
 
-    expect(errorButton).not.toBeEmptyDOMElement();
+    // https://testing-library.com/docs/react-testing-library/cheatsheet/#queries 참고!
+
+    // 만약 getBy / findBy api를 쓰면 찾을 수 없다는 에러가 나타나서 테스트 도중 break될것이다.
+
+    const errorButton = getQueryByRole("error-button");
+
+    expect(errorButton).not.toBeInTheDocument();
 
     // screen.debug();
   });
@@ -90,4 +95,8 @@ function findByElementByRole(roleName) {
 
 function findElementByText(text) {
   return screen.findByText(text);
+}
+
+function getQueryByRole(roleName) {
+  return screen.queryByRole(roleName);
 }
