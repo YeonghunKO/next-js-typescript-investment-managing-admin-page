e7c8e7ad20b78f20518bd9504515b5f86b87d8e4
Sinkyo <yhko1988@gmail.com>
2023-08-03 14:46:06 +0900

    feat  : 더 명확하게 에러 버튼 명시

--- components/molecules/ErrorModal/ErrorModal.tsx
+++ components/molecules/ErrorModal/ErrorModal.tsx
@@ -1,15 +1,15 @@
-import Button from '@components/atoms/Button';
-import { COLOR_VARIANTS } from '@constants/arguments';
-import useUnmountIfClickedOutside from '@hooks/common/useUnmountIfClickOutSide';
-import type { ErrorModalType } from '@type/molecules/ErrorModal';
-import React, { useRef } from 'react';
-import * as S from './ErrorModal.styles';
+import Button from "@components/atoms/Button";
+import { COLOR_VARIANTS } from "@constants/arguments";
+import useUnmountIfClickedOutside from "@hooks/common/useUnmountIfClickOutSide";
+import type { ErrorModalType } from "@type/molecules/ErrorModal";
+import React, { useRef } from "react";
+import * as S from "./ErrorModal.styles";
 
 const ErrorModal = ({ errorMessage, setErrorMessage }: ErrorModalType) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleCloseModal = () => {
-    setErrorMessage('');
+    setErrorMessage("");
   };
 
   useUnmountIfClickedOutside(modalRef, handleCloseModal);
@@ -18,7 +18,11 @@ const ErrorModal = ({ errorMessage, setErrorMessage }: ErrorModalType) => {
       <S.ModalContainer ref={modalRef}>
         <S.Container>
           <S.ErrorText>{errorMessage}</S.ErrorText>
-          <Button onClick={handleCloseModal} variant={COLOR_VARIANTS.SECONDARY}>
+          <Button
+            role="error-button"
+            onClick={handleCloseModal}
+            variant={COLOR_VARIANTS.SECONDARY}
+          >
             확인
           </Button>
         </S.Container>
